019ae06058106112b45cf1b54d088fe03eb7a192
ericrav <ericrabinowitz@me.com>
2017-01-03 11:10:02 -0500

    api auth type and agency name

--- index.js
+++ index.js
@@ -7,64 +7,49 @@ var twitterConfig = require('./twitter-config.js');
 console.log(Date());
 var Bot = new Twit(twitterConfig);
 
-var oauthOptions = {
+var yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD') + ' 00:00:00';
+var today = moment().format('YYYY-MM-DD') + ' 00:00:00';
+
+var searchOptions = {
   method: 'POST',
-  url: 'https://apis.accela.com/oauth2/token',
-  json: true,
+  url: 'https://apis.accela.com/v4/search/records/',
+  qs: { expand: 'addresses' },
   headers: {
-    'content-type': 'application/x-www-form-urlencoded',
-    'postman-token': 'ec06ed9b-fc6a-7390-994f-484e6b192319',
-    'cache-control': 'no-cache'
+    'postman-token': 'bae3fae3-45ad-5d31-0564-f7bd862da239',
+    'cache-control': 'no-cache',
+    'content-type': 'application/json',
+    'x-accela-appid': '636101632517218736',
+    'x-accela-agency': 'Atlanta_Ga',
+    'x-accela-environment': 'PROD'
   },
-  form: accelaConfig
+  body: {
+    address: { city: 'Atlanta' },
+    type: { subType: 'Beltline' },
+    openedDateFrom: yesterday,
+    openedDateTo: today
+  },
+  json: true
 };
 
-request(oauthOptions, function (error, response, body) {
-  if (error) console.error('error getting access token', error);
-
-  var accela_token = body.access_token;
-  var yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD') + ' 00:00:00';
-  var today = moment().format('YYYY-MM-DD') + ' 00:00:00';
-
-  var searchOptions = {
-    method: 'POST',
-    url: 'https://apis.accela.com/v4/search/records/',
-    qs: { expand: 'addresses' },
-    headers: {
-      'postman-token': 'bae3fae3-45ad-5d31-0564-f7bd862da239',
-       'cache-control': 'no-cache',
-       'content-type': 'application/json',
-       authorization:  accela_token
-    },
-    body: {
-      address: { city: 'Atlanta' },
-      type: { subType: 'Beltline' },
-      openedDateFrom: yesterday,
-      openedDateTo: today
-    },
-    json: true
-  };
-
-  request(searchOptions, function (error, response, body) {
-    if (error) console.error('error making search', error);
-    if (!body.result) {
-      console.log('no search results');
-      return; // no search results
-    }
-    
-    for (var i = 0; i < body.result.length; i++) {
-      var record = body.result[i];
-      var address = record.addresses[0].streetStart + ' ' + record.addresses[0].streetName + ' ' + 
-        (record.addresses[0].streetSuffix && record.addresses[0].streetSuffix.text ? record.addresses[0].streetSuffix.text : '');
-      var description = record.description;
-      console.log(record.id)
-      var status = (description || '').replace(/^\s+|\s+$/g, ''); // trim whitespace
-      if (status.length + address.length + ' : '.length <= 140) status += ((description ? ' : ' : '') + address);
-      else if (status.length > 140) status = status.slice(0,137) + '...';
-      console.log(status);
-      staggerTweet(address, status, i * 1000 * 60 * 3);
-    }
-  });
+request(searchOptions, function (error, response, body) {
+  if (error) console.error('error making search', error);
+  if (!body.result) {
+    console.log('no search results');
+    return; // no search results
+  }
+  
+  for (var i = 0; i < body.result.length; i++) {
+    var record = body.result[i];
+    var address = record.addresses[0].streetStart + ' ' + record.addresses[0].streetName + ' ' + 
+      (record.addresses[0].streetSuffix && record.addresses[0].streetSuffix.text ? record.addresses[0].streetSuffix.text : '');
+    var description = record.description;
+    console.log(record.id)
+    var status = (description || '').replace(/^\s+|\s+$/g, ''); // trim whitespace
+    if (status.length + address.length + ' : '.length <= 140) status += ((description ? ' : ' : '') + address);
+    else if (status.length > 140) status = status.slice(0,137) + '...';
+    console.log(status);
+    staggerTweet(address, status, i * 1000 * 60 * 3);
+  }
 });
 
 function staggerTweet(street, status, delay) {
